161e4afc7858d6777c1d5ca1078ed8ce171f7961
MatinSN <matinsoltany2000@gmail.com>
2020-07-17 10:23:08 +0430

    End of section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,35 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MyValueComponent } from './my-value/my-value.component';
-import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { AlertifyService } from './_services/alertify.service';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyValueComponent,
-    NavComponent,
-    HomeComponent,
-    RegisterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    BsDropdownModule.forRoot(),
-    BrowserAnimationsModule,
-  ],
-  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
-  bootstrap: [AppComponent],
+   declarations: [
+      AppComponent,
+      MyValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
-export class AppModule {}
+export class AppModule { }
